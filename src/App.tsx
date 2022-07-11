280c84a010a6e7dc81dcd546854bd28f517ae5bd
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-11 23:18:56 +0530

    set up initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { withDragAndDrop, Box } from "./Components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera, faImage } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [file, setFile] = useState<File | null>(null);
+
+	const handleFileChange = (files: FileList) => {
+		if (!files || files.length <= 0) return setFile(null);
+		const file = files[0];
+		setFile(file);
+	};
+
+	const DragAndDrop = withDragAndDrop(Box, handleFileChange);
+
+	return (
+		<div className="container">
+			<h1 className="text-white">Photo Art</h1>
+			<form className="form">
+				<DragAndDrop>
+					<span>Drag And Drop Files Here or Click</span>
+					<FontAwesomeIcon icon={faImage} size="2x" />
+				</DragAndDrop>
+				<Box onClick={e => console.log("Box Clicked")}>
+					<span>Take New Picture</span>
+					<FontAwesomeIcon icon={faCamera} size="2x" />
+				</Box>
+			</form>
+			{file && <img src={URL.createObjectURL(file)} alt={file.name} />}
+		</div>
+	);
+};
 
 export default App;
