fa070c7e963778d196439f79f79e27cd3b859a95
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-15 16:27:25 +0530

    move type declarations to .d.ts files

--- src/Types/index.d.ts
+++ src/Types/index.d.ts
@@ -1,1 +1,18 @@
-declare module "*.module.css";
+export interface ImageState {
+	image: string;
+}
+
+export enum ImageActionType {
+	SET = "SET_IMAGE",
+	CLEAR = "CLEAR_IMAGE"
+}
+
+export interface ImageAction {
+	type: ImageActionType;
+	payload: string;
+}
+
+export interface ImageContextType {
+	state: ImageState;
+	dispatch: React.Dispatch<ImageAction>;
+}
