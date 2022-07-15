20a94d51e1d2b51097385cb634c6d4b846df4dce
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-15 16:25:44 +0530

    add ImageProvider

---
+++ src/ImageProvider.tsx
@@ -0,0 +1,34 @@
+import React, { useReducer, createContext, useContext } from "react";
+import { ImageState, ImageAction, ImageActionType, ImageContextType } from "./Types/index";
+
+const initialState: ImageState = {
+	image: ""
+};
+
+const ImageContext = createContext<ImageContextType>({} as ImageContextType);
+
+export const useImage = (): ImageContextType | null => useContext(ImageContext);
+
+export const ImageProvider = ({ children }: React.PropsWithChildren) => {
+	const reducer = (state: ImageState, { type, payload }: ImageAction): ImageState => {
+		switch (type) {
+			case ImageActionType.SET:
+				return { ...state, image: payload };
+			case ImageActionType.CLEAR:
+				return { ...state, image: "" };
+		}
+	};
+
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<ImageContext.Provider
+			value={{
+				state,
+				dispatch
+			}}
+		>
+			{children}
+		</ImageContext.Provider>
+	);
+};
