d60d4d678aa1c57aa678e7b3388adcc147638c77
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-16 14:19:29 +0530

    export modal

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,5 +1,6 @@
 import Box from "./Box/Box";
 import withDragAndDrop from "./withDragAndDrop";
 import Header from "./Header/Header";
+import Modal from "./Modal/Modal";
 
 export { Box, withDragAndDrop, Header };
