aa480642d317427cce1bee30ed094a1b57a24a62
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-19 20:29:29 +0530

    modify Header Component for reuse

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,36 +1,16 @@
-import { faCameraRetro, faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useTheme } from "../../Context/ThemeProvider";
+import { combineWithTheme } from "../../utils";
 import styles from "./Header.module.css";
 
-const Header = () => {
-	const { theme, setTheme } = useTheme();
+type HeaderBackgroundType = "transparent" | "light" | "dark";
 
-	const switchTheme = () => {
-		setTheme(prev => {
-			if (prev === "dark") return "light";
-			return "dark";
-		});
-	};
+interface Props extends React.PropsWithChildren {
+	background: HeaderBackgroundType;
+}
 
+const Header = ({ children, background }: Props) => {
 	return (
-		<header>
-			<div className={styles.header}>
-				<div></div>
-				<div style={{ display: "flex", alignItems: "flex-end", gap: "10px" }}>
-					<FontAwesomeIcon icon={faCameraRetro} size="3x" />
-					<h1>Photo Art</h1>
-				</div>
-				<div>
-					<button onClick={switchTheme}>
-						{theme === "dark" ? (
-							<FontAwesomeIcon icon={faSun} size="2x" />
-						) : (
-							<FontAwesomeIcon icon={faMoon} size="2x" />
-						)}
-					</button>
-				</div>
-			</div>
+		<header className={styles[combineWithTheme(background, "header")]}>
+			<div className={styles.header}>{children}</div>
 		</header>
 	);
 };
