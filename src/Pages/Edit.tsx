651c8a36f7a21a412febbab3515a2a21db31e20b
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-07-20 16:49:09 +0530

    Modify Home and Edit Pages

--- src/Pages/Edit.tsx
+++ src/Pages/Edit.tsx
@@ -1,8 +1,101 @@
+import { useState, useEffect } from "react";
+import { Header } from "../Components";
+import { useImage } from "../Context/ImageProvider";
+import { useTheme } from "../Context/ThemeProvider";
+import { combineWithTheme } from "../utils";
+
+interface FilterProperties {
+	name: string;
+	property: string;
+	value: number;
+	range: { min: number; max: number };
+	unit: string;
+}
+
+const DEFAULT_PROPERTIES: FilterProperties[] = [
+	{
+		name: "Brightness",
+		property: "brightness",
+		value: 100,
+		range: {
+			min: 0,
+			max: 200
+		},
+		unit: "%"
+	},
+	{
+		name: "Brightness",
+		property: "contrast",
+		value: 100,
+		range: {
+			min: 0,
+			max: 200
+		},
+		unit: "%"
+	},
+	{
+		name: "Saturation",
+		property: "saturate",
+		value: 100,
+		range: {
+			min: 0,
+			max: 200
+		},
+		unit: "%"
+	},
+	{
+		name: "Sepia",
+		property: "sepia",
+		value: 0,
+		range: {
+			min: 0,
+			max: 100
+		},
+		unit: "%"
+	},
+	{
+		name: "Hue Rotate",
+		property: "hue-rotate",
+		value: 0,
+		range: {
+			min: 0,
+			max: 360
+		},
+		unit: "deg"
+	},
+	{
+		name: "Blur",
+		property: "blur",
+		value: 0,
+		range: {
+			min: 0,
+			max: 20
+		},
+		unit: "px"
+	}
+];
+
 const Edit = () => {
+	const [options, setOptions] = useState<FilterProperties[]>(DEFAULT_PROPERTIES);
+	const { state } = useImage();
+	const { theme } = useTheme();
+
+	const { image, name } = state;
+
+	useEffect(() => {
+		document.title = name;
+	}, [name]);
+
 	return (
-		<div>
-			<h1>Edit</h1>
-		</div>
+		<section id={combineWithTheme(theme, "Edit")}>
+			<Header background={theme === "dark" ? "dark" : "light"}>
+				<strong>{name}</strong>
+				<button>Save</button>
+			</Header>
+			<div className="edit-img-comtainer">
+				<img src={image} alt={name} className="edit-img" />
+			</div>
+		</section>
 	);
 };
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,7 @@ import "../App.css";
 import { withDragAndDrop, Box, Header } from "../Components";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera, faImage } from "@fortawesome/free-solid-svg-icons";
+import { faCamera, faCameraRetro, faImage, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { useImage } from "../Context/ImageProvider";
 import { ImageActionType } from "../Types";
 import { useTheme } from "../Context/ThemeProvider";
@@ -11,22 +11,44 @@ import { combineWithTheme } from "../utils";
 const Home = () => {
 	const { dispatch } = useImage();
 	const navigate = useNavigate();
-	const { theme } = useTheme();
+	const { theme, setTheme } = useTheme();
 
 	const handleFileChange = (files: FileList) => {
 		if (!files || files.length <= 0) return;
 		const file = files[0];
 		const imageUrl = URL.createObjectURL(file);
 		dispatch({ type: ImageActionType.SET, payload: { image: imageUrl, name: file.name } });
-		navigate("/");
+		navigate("/edit");
 	};
 
 	const DragAndDrop = withDragAndDrop(Box, handleFileChange);
 
+	const switchTheme = () => {
+		setTheme(prev => {
+			if (prev === "dark") return "light";
+			return "dark";
+		});
+	};
+
 	return (
 		<section id="Home">
 			<div className="container">
-				<Header />
+				<Header background="transparent">
+					<div></div>
+					<div style={{ display: "flex", alignItems: "flex-end", gap: "10px" }}>
+						<FontAwesomeIcon icon={faCameraRetro} size="3x" style={{ color: "white" }} />
+						<h1 style={{ color: "white" }}>Photo Art</h1>
+					</div>
+					<div>
+						<button onClick={switchTheme} className="btn-borderless">
+							{theme === "dark" ? (
+								<FontAwesomeIcon icon={faSun} size="2x" />
+							) : (
+								<FontAwesomeIcon icon={faMoon} size="2x" />
+							)}
+						</button>
+					</div>
+				</Header>
 				<form className={`bg ${combineWithTheme(theme, "bg")}`}>
 					<DragAndDrop>
 						<span>Drag And Drop Files Here or Click</span>
